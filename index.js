7a4dfdbfcf6619670d39fb546314bfd794901d8b
Parveen Singh <parveen.singh@cloudacademy.com>
2021-06-03 22:39:25 -0700

    Adding Env variable

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const http = require('http');
 const slot = process.env.WEBSLOT || "Staging"
+const resp = `Good Job! This App is deployed to the ${slot} Slot first.`
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Good Job! This App is deployed to the ", slot, " Slot first.");
+    response.end(resp);
 });
 
 const port = process.env.PORT || 1337;
