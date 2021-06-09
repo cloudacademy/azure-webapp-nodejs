007efbb0bc993de4be9d95a170fa30ba12ad9719
Parveen Singh <parveen.singh@cloudacademy.com>
2021-06-09 14:31:27 -0700

    Updating Output

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const slot = process.env.WEBSLOT || "Staging"
-const resp = `Good Job! This App is deployed to the ${slot} Slot first.`
+const resp = `Good Job! This App is deployed to the ${slot} Slot.`
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end(resp);
